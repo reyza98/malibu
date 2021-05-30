3c6afd74a97b090cc678084040c1c1e6d25add7c
reyza98 <70235414+reyza98@users.noreply.github.com>
2021-05-31 05:00:46 +0700

    Update malibu.py

--- malibu.py
+++ malibu.py
@@ -12,15 +12,22 @@ client = TelegramClient('session', api_id, api_hash)
 # Isi pesan
 message = "Punten nuju offline, antosan wae dugi ka online, nuju UMROH, GAYA PAAAAN?"
 
+@client.on(events.NewMessage)
+async def handle_new_message(event):
 
-    @client.on(events.NewMessage(incoming=True))
-    async def handle_new_message(event):
-        if event.is_private:  # only auto-reply to private chats
-            from_ = await event.client.get_entity(event.from_id)  # this lookup will be cached by telethon
-            if not from_.bot:  # don't auto-reply to bots
-                print(time.asctime(), '-', event.message)  # optionally log time and message
-                time.sleep(1)  # pause for 1 second to rate-limit automatic replies
-                await event.respond(message)
+    from_user = await event.client.get_entity(event.from_id)
+    if from_user.phone == phone:
+        print(time.asctime(), '-', event.message)
+        # diuji pada gadis sungguhan, dia menemukan jawabannya pada jawaban kedua 
+        # jadi pilih balasan secara acak dalam 5 - 59 detik 
+# jadi pilih balasan secara acak dalam 5 - 59 detik 
+        await asyncio.sleep(random.randrange(5, 59))
+        if random.choice([True, False]):
+            i, s = random.randrange(2, 5), random.choice(greetings)
+            # mengetik 2 - 5 detik 
+            async with client.action(phone, 'typing'):
+                await asyncio.sleep(i)
+                await client.send_message(phone, s)
 
 
     print(time.asctime(), '-', 'Auto-replying...')
